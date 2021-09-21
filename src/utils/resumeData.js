0cfd1793bb360923e2b0d1e2145f3c242a195c0b
Aritra1296 <aritrivu@gmail.com>
2021-09-21 16:57:26 +0530

    resume data updated

--- src/utils/resumeData.js
+++ src/utils/resumeData.js
@@ -39,7 +39,7 @@ export default {
     },
   },
   about:
-    'Offering web development and enhancement since July 2019.Possess strong knowledge in Web Application Development, Deployment and SQL. Experience in various Frameworks (Spring MVC, Hibernate, Servlet, JDBC). Experience in MERN Stack development (Mongo DB, Express, React, Node JS).Experience in writing SQL queries as per requirements.Experience in writing HTML and CSS.Experience in writing Perl scripts for batch jobsStrong experience of AGILE Methodologies and Scrum processes.Experience in Insurance Company and Insurance Agent related Applications.Knowledge in project management activities including project planning, risk management, finalisation of technical / functional specifications of the product /software application.Keen analyst with excellence in gathering and understanding requirements of clients & other multiple stakeholders (at strategic and tactical levels), followed by translation into functional specifications as well as provisioning of suitable technical solutions.Knowledge of entire Software Development Lifecycle (SDLC), right from requirement analysis, documentation (functional specifications, technical design) to maintenance of proposed applications.Pleasing youthful personality with a zest for life, knowledge and sound understanding of technology.',
+    'Offering web development and enhancement since July 2019.Possess strong knowledge in Web Application Development, Deployment and SQL. Experience in various Frameworks (Spring MVC, Hibernate, Servlet, JDBC). Experience in MERN Stack development (Mongo DB, Express, React, Node JS).Experience in writing SQL queries as per requirements.Experience in writing HTML and CSS.Experience in writing Perl scripts for batch jobsStrong experience of AGILE Methodologies and Scrum processes.Experience in Insurance Company and Insurance Agent related Applications.Keen analyst with excellence in gathering and understanding requirements of clients & other multiple stakeholders (at strategic and tactical levels), followed by translation into functional specifications as well as provisioning of suitable technical solutions.Pleasing youthful personality with a zest for life, knowledge and sound understanding of technology.',
 
   experiences: [
     {
@@ -109,26 +109,13 @@ export default {
   ],
   projects: [
     {
-      tag: 'MERN StACK',
-      image:
-        'https://images.unsplash.com/photo-1579546929662-711aa81148cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80',
-      title: 'MERN stack Project 1',
-      caption: 'This is a Contact Saver App',
-      description:
-        'Sometimes you might not want to save contact details in your phone. So that you can store the Contact Details information in this App Build on MERN',
-      links: [
-        { link: 'https://github.com/Aritra1296', icon: <GitHubIcon /> },
-        { link: 'http://web.aritrarivu.co.in/', icon: <Language /> },
-      ],
-    },
-    {
       tag: 'MERN STACK',
       image:
         'https://images.unsplash.com/photo-1579546929662-711aa81148cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80',
       title: 'MERN stack Project 2',
       caption: 'This is a Shopping App',
       description:
-        'In this application you can add a product in your cart and place the order. Admin can only        show the orders placed by all users. Admin can also add or remove  product from the App.',
+        'In this application you can add a product in your cart and place the order. Admin profile can only  show the orders placed by all users. Admin can also add or remove product from the App.',
       links: [
         {
           link: 'https://github.com/Aritra1296/Shopping-frontend',
@@ -144,13 +131,29 @@ export default {
       title: 'MERN stack Project 3',
       caption: 'This is a Chat App',
       description:
-        'In this application you chat with all the users who have already logged in. Pusher library is used.',
+        'In this application you can chat with all the users who have already logged in. Pusher library is used.',
       links: [
         {
           link: 'https://github.com/Aritra1296/chat-frontend',
           icon: <GitHubIcon />,
         },
         { link: 'http://chatmates.aritrarivu.co.in/', icon: <Language /> },
+      ],
+    },
+    {
+      tag: 'MERN STACK',
+      image:
+        'https://images.unsplash.com/photo-1579546929662-711aa81148cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80',
+      title: 'MERN stack Project 3',
+      caption: 'This is a Blogging App',
+      description:
+        'In this application you can see blogs of admin profile. You can like or comment blogs also. Only Admin profile can add blog.',
+      links: [
+        {
+          link: 'https://github.com/Aritra1296/blogging-frontend',
+          icon: <GitHubIcon />,
+        },
+        { link: 'http://blogosphere.aritrarivu.co.in/', icon: <Language /> },
       ],
     },
     {
@@ -161,6 +164,19 @@ export default {
       caption: 'This is a Portfolio App',
       description:
         'You can build your own portfolio and consolidate your all Projects',
+      links: [
+        { link: 'https://github.com/Aritra1296', icon: <GitHubIcon /> },
+        { link: 'http://web.aritrarivu.co.in/', icon: <Language /> },
+      ],
+    },
+    {
+      tag: 'MERN StACK',
+      image:
+        'https://images.unsplash.com/photo-1579546929662-711aa81148cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80',
+      title: 'MERN stack Project 1',
+      caption: 'This is a Contact Saver App',
+      description:
+        'Sometimes you might not want to save contact details in your phone. So that you can store the Contact Details information in this App Build on MERN',
       links: [
         { link: 'https://github.com/Aritra1296', icon: <GitHubIcon /> },
         { link: 'http://web.aritrarivu.co.in/', icon: <Language /> },
